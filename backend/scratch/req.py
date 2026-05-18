4bee7434981f63f01fdad54a8a0729339bcd1964
Nishankyk <nishankkansara@gmail.com>
2026-05-19 04:27:15 +0530

    Update requirements

--- backend/scratch/req.py
+++ backend/scratch/req.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://localhost:8000/edit"
+url = "https://pdf-editor-0eg9.onrender.com/edit"
 data = {
     "file_id": "aa1e067d-1011-4c9b-ae36-98e1365b575b",
     "instruction": "Can you add the black background to the file and give me the final?"
